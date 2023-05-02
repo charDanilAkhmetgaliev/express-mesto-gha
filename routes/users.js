28843f5e090b4332e88bb6d07599eaadc23d010d
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-05-02 13:49:34 +0300

    feature: создать роуты для регистрации и авторизации

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
 const router = require('express').Router();
 
 const {
-  getUsers, getUsersById, createUser, updateData, updateAvatar,
+  getUsers, getUsersById, createUser, updateData, updateAvatar, login,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 
 router.get('/:id', getUsersById);
 
-router.post('/', createUser);
-
 router.patch('/me', updateData);
 
 router.patch('/me/avatar', updateAvatar);
+
+router.post('/signin', login);
+
+router.post('/signup', createUser);
 
 module.exports = router;
